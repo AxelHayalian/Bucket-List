beab8f5394e162fc043cea518f342effc63dfe54
AxelHayalian <axel.hayalian2023@campus-eni.fr>
2024-02-28 19:17:53 +0100

    Création contrôlleur et page twing
    Mise place d'url et de fichier css

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
+
 
 class MainController extends AbstractController
 {
     #[Route(path: '/', name: 'main_home')]
+
     public function home (): Response
     {
         return $this->render('main/home.html.twig');
